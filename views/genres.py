967af71279cf348e31a8fccd1bf9879e70e105a1
elecr0n4ik <andreyshka3@gmail.com>
2023-02-25 09:51:39 +0300

    step 3

--- views/genres.py
+++ views/genres.py
@@ -1,5 +1,5 @@
 from flask_restx import Resource, Namespace
-
+from flask import request
 from dao.model.genre import GenreSchema
 from implemented import genre_service
 
@@ -13,6 +13,11 @@ class GenresView(Resource):
         res = GenreSchema(many=True).dump(rs)
         return res, 200
 
+    def post(self):
+        req_json = request.json
+        genre = genre_service.create(req_json)
+        return "", 201, {"location": f"/genres/{genre.id}"}
+
 
 @genre_ns.route('/<int:rid>')
 class GenreView(Resource):
@@ -20,3 +25,14 @@ class GenreView(Resource):
         r = genre_service.get_one(rid)
         sm_d = GenreSchema().dump(r)
         return sm_d, 200
+
+    def put(self, rid):
+        req_json = request.json
+        if "id" not in req_json:
+            req_json["id"] = rid
+        genre_service.update(req_json)
+        return "", 204
+
+    def delete(self, bid):
+        genre_service.delete(bid)
+        return "", 204
--- views/movies.py
+++ views/movies.py
@@ -1,6 +1,5 @@
 from flask import request
 from flask_restx import Resource, Namespace
-
 from dao.model.movie import MovieSchema
 from implemented import movie_service
 
